81d3ff34f79896c14888b4831a6836bda8bbbb17
Prasant Chandra Poddar <chandraprasant.pcp@gmail.com>
2024-01-18 23:30:46 +0530

    solved

---
+++ Strings (Practice set-1)/Problem31.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+int main() {
+    char str[30];
+    char word[30];
+
+    // user input
+    printf("Enter a string: ");
+    gets(str);
+    printf("Enter the word: ");
+    gets(word);
+
+    // finding the length of the string and word
+    int strlen = 0;
+    int wrdlen = 0;
+    while (str[strlen] != '\0') {
+        strlen++;
+    }
+
+    while (word[wrdlen] != '\0') {
+        wrdlen++;
+    }
+
+    // checking the first occurrance of word
+    int count = 0;
+    for (int i = 0; i <= strlen-wrdlen; i++) {
+        if (str[i] == word[0]) { // checking the first character
+            char isEqual = 't';
+
+            if ((i > 0 && str[i-1] != ' ') // checking the space around word
+            || (i+wrdlen < strlen && str[i+wrdlen] != ' ')) {
+                continue;
+            }
+
+            int j = i+1;
+            while ( j < wrdlen) {
+                if (str[i+j] != word[j]) {
+                    isEqual = 'f';
+                }
+
+                j++;
+            }
+
+            if (isEqual == 't') {
+                count++;
+            }
+        }
+    }
+    
+    printf("The all occurrences of %s in %s: %d", word, str, count);
+
+    return 0;
+}
