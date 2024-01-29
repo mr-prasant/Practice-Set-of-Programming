ec8dc514c2eb446a2f63c85f6e31bb4b324cba00
Prasant Chandra Poddar <chandraprasant.pcp@gmail.com>
2024-01-29 18:39:47 +0530

    solved

--- Pattern(Practice set-1)/Pattern01.java
+++ Pattern(Practice set-1)/Pattern01.java
@@ -11,7 +11,7 @@ public class Pattern01 {
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                System.out.print((j+1) + " ");
+                System.out.print("* ");
             }
             System.out.println();
         }
--- Pattern(Practice set-1)/Pattern04.java
+++ Pattern(Practice set-1)/Pattern04.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
 
-public class Pattern02 {
+public class Pattern04 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        
         // user input
         System.out.print("Enter a number: ");
         int n = sc.nextInt();
@@ -11,9 +11,10 @@ public class Pattern02 {
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                System.out.print((j+1) + " ");
+                System.out.print((char) (65+(j%26)) + " ");
             }
             System.out.println();
         }
+
     }
 }
