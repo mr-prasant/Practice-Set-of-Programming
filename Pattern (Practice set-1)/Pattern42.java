dd83a5584eef67d7e8dd5a872da0b24c9ef1ac66
Prasant Chandra Poddar <chandraprasant.pcp@gmail.com>
2024-02-20 00:54:18 +0530

    solved

---
+++ Pattern (Practice set-1)/Pattern42.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Pattern42 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        // user input
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        sc.close();
+
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = n-1; j >= 0; j--) {
+                if (j > i) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print((char) (97 + count++) + " ");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+}
